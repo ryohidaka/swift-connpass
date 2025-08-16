83a10e1e3b64b881aebca6e462d9a24f405932ad
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-08-16 12:25:31 +0900

    feat: Frameworkを新規作成

---
+++ connpass/connpass.swift
@@ -0,0 +1,10 @@
+//
+//  connpass.swift
+//  connpass
+//  
+//  Created by ryohidaka on 2025/08/16
+//  
+//
+
+import Foundation
+
---
+++ connpassTests/connpassTests.swift
@@ -0,0 +1,18 @@
+//
+//  connpassTests.swift
+//  connpassTests
+//  
+//  Created by ryohidaka on 2025/08/16
+//  
+//
+
+import Testing
+@testable import connpass
+
+struct connpassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
